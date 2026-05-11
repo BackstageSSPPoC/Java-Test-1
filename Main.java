731d44625fabf44acc6afddd3ff37b570db94b4d
Chaitanya Shekhar Pandey <113713770+Chaitanya-Shekhar-Pandey@users.noreply.github.com>
2026-05-11 13:31:21 +0530

    Implement basic HTTP server with two routes

---
+++ Main.java
@@ -0,0 +1,39 @@
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        int port = 8080;
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+
+        // Route: /
+        server.createContext("/", exchange -> {
+            String response = "{\"status\":\"UP\", \"message\":\"Hello from Java!\"}";
+            sendResponse(exchange, response);
+        });
+
+        // Route: /hello
+        server.createContext("/hello", exchange -> {
+            String response = "{\"message\":\"Hello World from Java App!\"}";
+            sendResponse(exchange, response);
+        });
+
+        server.start();
+        System.out.println("Server running on port " + port);
+    }
+
+    static void sendResponse(HttpExchange exchange, String response) throws IOException {
+        exchange.getResponseHeaders().set("Content-Type", "application/json");
+        exchange.sendResponseHeaders(200, response.length());
+        OutputStream os = exchange.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
+    }
+}
